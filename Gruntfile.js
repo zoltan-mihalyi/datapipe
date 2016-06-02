af7ce345467caeeb7824d5f6d57b6c45522edb84
zoltan-mihalyi <zoltan.mihalyi@webvalto.hu>
2016-06-02 11:58:53 +0200

    coverage reports

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,49 @@
 module.exports = function(grunt) {
 
     grunt.initConfig({
-        jasmine_nodejs: {
+        jasmine_node: {
             options: {
-                specNameSuffix: '.js'
+                showColors: true,
+                forceExit: true,
+                match: '.',
+                matchAll: false,
+                specFolders: ['test'],
+                extensions: 'js',
+                specNameMatcher: '',
+                captureExceptions: true
             },
             test: {
-                specs: [
-                    "test/**"
-                ]
+                options: {
+                    coverage: {
+                        report: [],
+                        excludes: ['test/**']
+                    }
+                },
+                src: ['**/*.js']
+            },
+            testFast: {
+                options: {
+                    coverage: {
+                        excludes: ['**']
+                    }
+                },
+                src: ['**/*.js']
+            }
+        },
+        remapIstanbul: {
+            build: {
+                src: 'coverage/coverage.json',
+                options: {
+                    reports: {
+                        'html': 'coverage/html-report',
+                        'json': 'coverage/coverage-ts.json'
+                    }
+                }
             }
         },
         clean: {
-            dist: ['dist']
+            dist: ['dist'],
+            coverage: ['coverage']
         },
         ts: {
             compile: {
@@ -29,11 +60,13 @@ module.exports = function(grunt) {
         }
     });
 
-    grunt.loadNpmTasks('grunt-jasmine-nodejs');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-ts');
     grunt.loadNpmTasks('grunt-tslint');
+    grunt.loadNpmTasks('grunt-jasmine-node-coverage');
+    grunt.loadNpmTasks('remap-istanbul');
 
-    grunt.registerTask('test', ['jasmine_nodejs']);
+    grunt.registerTask('test-fast', ['jasmine_node:testFast']);
+    grunt.registerTask('test', ['clean:coverage', 'jasmine_node:test', 'remapIstanbul']);
     grunt.registerTask('build', ['clean', 'ts']);
 };
