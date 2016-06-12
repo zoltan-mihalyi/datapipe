ab45d29c889aff9ce790bf757301050404ba0931
zoltan-mihalyi <zoltan.mihalyi@webvalto.hu>
2016-06-12 23:44:09 +0200

    contains benchmark

---
+++ benchmarks/tests/contains.js
@@ -0,0 +1,38 @@
+var array = require('../array');
+var _ = require('underscore');
+var __ = require('lodash');
+var dp = require('../../dist/datapipe');
+
+var _contains = _.contains;
+var __contains = __.includes;
+
+var obj = array[3];
+
+var fn = dp('array').contains(obj).fn();
+var nativeFn = function(array) {
+    var length = array.length;
+    for (var i = 0; i < length; ++i) {
+        if (array[i] === obj) {
+            return true;
+        }
+    }
+    return false;
+};
+
+module.exports = {
+    name: 'contains',
+    tests: {
+        native: function() {
+            return nativeFn(array);
+        },
+        undersorcery: function() {
+            return fn(array);
+        },
+        underscore: function() {
+            return _contains(array, obj);
+        },
+        lodash: function() {
+            return __contains(array, obj);
+        }
+    }
+};
