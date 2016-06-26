f8dec399153b6c095f84d11b342ccfeb637b7527
zoltan-mihalyi <zoltan.mihalyi@webvalto.hu>
2016-06-26 08:12:26 +0200

    rest benchmarks

---
+++ benchmarks/tests/rest.js
@@ -0,0 +1,36 @@
+var array = require('../array');
+var _ = require('underscore');
+var __ = require('lodash');
+var dp = require('../../dist/datapipe');
+
+var _rest = _.rest;
+var __rest = __.drop;
+
+
+var fn = dp('array').rest(3).fn();
+var nativeFn = function(array) {
+    var length = array.length;
+    var result = new Array(length <= 3 ? 0 : length - 3);
+    for (var i = 3; i < length; ++i) {
+        result[i - 3] = array[i];
+    }
+    return result;
+};
+
+module.exports = {
+    name: 'map',
+    tests: {
+        native: function() {
+            return nativeFn(array);
+        },
+        undersorcery: function() {
+            return fn(array);
+        },
+        underscore: function() {
+            return _rest(array, 3);
+        },
+        lodash: function() {
+            return __rest(array, 3);
+        }
+    }
+};
